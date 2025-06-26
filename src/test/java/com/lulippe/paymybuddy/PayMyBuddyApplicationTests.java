08af4d446ea0b30952d6ee9f83abbecc29a07e66
Caroline de La Brosse <caroline.delabrosse@gmail.com>
2025-06-26 11:49:23 +0200

    Initial commit (PMB-0): project Pay My Buddy

---
+++ src/test/java/com/lulippe/paymybuddy/PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lulippe.paymybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
